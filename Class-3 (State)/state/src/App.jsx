b5cd6c0613de637224c8557e9d0176dd77f26eee
mrinal1224 <iammrinalkc@gmail.com>
2026-02-09 11:59:54 +0530

    handle a form

--- Class-3 (State)/state/src/App.jsx
+++ Class-3 (State)/state/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Counter from './components/Counter'
+import Form from './components/Form'
 
 function App() {
 
 
   return (
     <div>
-       <Counter/>
+       {/* <Counter/> */}
+       <Form/>
+
     </div>
   )
 }
---
+++ Class-3 (State)/state/src/components/Form.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Form() {
+  return (
+    <div>
+      <h1>Sign Up Form</h1>
+
+      <label>Name</label>
+      <input type="text" />
+
+      <label>Email</label>
+      <input type="text" />
+
+      <label>Password</label>
+      <input type="text" />
+
+      <button>Submit</button>
+    </div>
+  );
+}
+
+export default Form;
